b059c751519f36ae4c43fd22cda9851d187513e8
Dagal <dejarding@gmail.com>
2016-01-30 10:39:23 +0100

    Starting block

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NeuronTest
 Testing Neural Network with C++
+-------------------------------
+* First create just a neuron and trying to use it
+
---
+++ src/NeuronTest.cpp
@@ -0,0 +1,16 @@
+/**
+ * Filename : NeuronTest.cpp
+ *
+ * Description : Main Program
+ *
+ **/
+
+#include <iostream>
+#include "NeuronTestConfig.h"
+
+int main(int    argc,
+         char **argv)
+{
+  std::cout << "NeuronTest V" << NeuronTest_VERSION_MAJOR << "." << NeuronTest_VERSION_MINOR << std::endl;
+  return 0;
+} 
